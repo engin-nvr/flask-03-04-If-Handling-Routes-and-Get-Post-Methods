70ce6df200c8adad8595e41df766d46f71761a80
engin17 <unverengin17@gmail.com>
2024-03-18 22:51:45 +0300

    second

--- flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
+++ flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
@@ -8,23 +8,26 @@ app = Flask(__name__)
 # Create welcome page with main.html file and assign it to the root path
 @app.route('/')
 def home():
-    return render_template('main.html', name='osvaldo')
+    return render_template('main.html', name='engin')
 
-# Write a function named `greet` which uses template file named `greet.html` given under 
-# `templates` folder. it takes parameters from query string on URL, assign that parameter 
+# Write a function named `greet` which uses template file named `greet.html` given under
+# `templates` folder. it takes parameters from query string on URL, assign that parameter
 # to the 'user' variable and sent that user name into the html file. If it doesn't have any parameter, warning massage is raised
 
+
 @app.route('/greet', methods=['GET'])
-def greet(): 
-   if 'user' in request.args: 
+def greet():
+    if 'user' in request.args:
         usr = request.args['user']
         return render_template('greet.html', user=usr)
-   else:
+    else:
         return render_template('greet.html', user='Send your user name with "greet?user=xxxxx" param in query string')
 
-# Write a function named `login` which uses `GET` and `POST` methods, 
-# and template files named `login.html` and `secure.html` given under `templates` folder 
+# Write a function named `login` which uses `GET` and `POST` methods,
+# and template files named `login.html` and `secure.html` given under `templates` folder
 # and assign to the static route of ('login')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -33,9 +36,10 @@ def login():
         if password == 'clarusway':
             return render_template('secure.html', user=user_name.title())
         else:
-            return render_template('login.html', user=user_name.title(), control = True)
+            return render_template('login.html', user=user_name.title(), control=True)
     else:
-        return render_template('login.html', control = False)
+        return render_template('login.html', control=False)
+
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
